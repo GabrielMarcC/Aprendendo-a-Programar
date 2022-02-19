d16cc57dbb0a817e6d168cf305ccbf64d236237b
Gabriel M <gabrielmarcelino099@gmail.com>
2022-02-19 17:28:41 -0300

    aula12

---
+++ aula12.ex/ex015/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 23
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        //BOM DIA!
+        img.src = 'foto-manha.png'
+        document.body.style.background = '#FFA500'
+    }else if(hora >= 12 && hora <18){
+        //BOA TARDE!
+        img.src = 'foto-tarde.png'
+        document.body.style.background = '#b9846f'
+    }else{
+        //Boa Noite
+        img.src = 'foto-noite.png'
+        
+    }
+}
