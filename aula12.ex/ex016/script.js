d7a83fcfe2d4342f708d1c8ba0c68d46fe3d5152
Gabriel M <gabrielmarcelino099@gmail.com>
2022-02-20 16:40:50 -0300

    pratica-aula-12

---
+++ aula12.ex/ex016/script.js
@@ -0,0 +1,48 @@
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fAno = document.getElementById('txtano')
+   var res = document.querySelector('div#res')
+   if (fAno.value.length == 0 || fAno.value > ano){
+       window.alert('[ERRO] Verifique seus dados e tente novamente')
+   } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src','foto-bebe-m.png')
+            }else if(idade <= 21){
+                //jovem
+                img.setAttribute('src','foto-jovem-m.png')
+            }else if(idade <= 50){
+                //adulto
+                img.setAttribute('src','foto-adulto-m.png')
+            }else{
+                //idoso
+                img.setAttribute('src','foto-idoso-m.png')
+            }
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade  >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            }else if(idade <= 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            }else if(idade <= 50){
+                //adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
+   }
+}
