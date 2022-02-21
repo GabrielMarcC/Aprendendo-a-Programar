2696de7da4092e08e03905f98ab0dc29f0bde8ba
Gabriel M <gabrielmarcelino099@gmail.com>
2022-02-21 16:23:36 -0300

    aula14

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var x = 2
+var y = 1
+
+/*while( x <= 7){
+    console.log('Olá, tudo bem?')
+    x ++
+}*/
+
+do{
+    m = y * x
+    console.log(x,'x', y,'=',m)
+    y ++
+}while(y <= 10)
+
+
+
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+x = 1
+while(x <= 10){
+    console.log(x)
+    x++
+}
+*/
+
+var n = 5
+for(c=1; c <= 10; c++){
+    console.log(n,'x',c,'=',c)
+    r = n * c
+}
